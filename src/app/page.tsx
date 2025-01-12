d818d4eb28a0db1f799d3666380709b801e538b0
Richmond Baltazar <baltazarrichmond31@gmail.com>
2025-01-12 21:17:32 +0800

    removed images folder and its content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 import "@/app/globals.css";
-import Image from "next/image";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div className="flex items-center justify-center h-screen flex-col">
-      <Image
-        src={"/images/baskitty_logo.png"}
-        alt="Baskitty Logo"
-        width={200}
-        height={150}
-      />
       <h1 className="font-semibold text-4xl">Welcome to Baskitty</h1>
       <div className="flex gap-10">
         <Link href={{}} className="text-2xl ">
